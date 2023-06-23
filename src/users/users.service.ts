92771de440bf46d562e47f3e722fb6a867cea8e6
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-06-23 19:02:50 -0300

    feat: add route to create user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { ConflictException } from '@nestjs/common/exceptions';
+import { PrismaService } from 'src/database/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return createUserDto;
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const emailTaken = await this.prismaService.user.findUnique({
+      where: { email: createUserDto.email },
+    });
+    if (emailTaken) throw new ConflictException('This email is already in use');
+
+    const user = await this.prismaService.user.create({
+      data: createUserDto,
+    });
+    return user;
   }
 }
