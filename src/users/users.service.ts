020b285ec0a55f81ecce9815de99905304d2ba10
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-06-27 11:21:36 -0300

    feat: encode user password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConflictException } from '@nestjs/common/exceptions';
+import { hash } from 'bcryptjs';
 import { PrismaService } from 'src/database/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
@@ -8,14 +9,19 @@ export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async create(createUserDto: CreateUserDto) {
+    const { name, email, password } = createUserDto;
+
     const emailTaken = await this.prismaService.user.findUnique({
-      where: { email: createUserDto.email },
+      where: { email: email },
+      select: { id: true },
     });
     if (emailTaken) throw new ConflictException('This email is already in use');
 
+    const hashedPassword = await hash(password, 12);
     const user = await this.prismaService.user.create({
-      data: createUserDto,
+      data: { name, email, password: hashedPassword },
     });
-    return user;
+
+    return { name: user.name, email: user.email };
   }
 }
