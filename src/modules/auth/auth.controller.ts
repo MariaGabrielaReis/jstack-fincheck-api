4a4d35187ae1be0b05050cfda84fb4857a76a773
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-07-18 17:43:53 -0300

    feat: create decorator to make routes public

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post, SetMetadata } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { IsPublic } from 'src/shared/decorators/IsPublic';
 import { AuthService } from './auth.service';
 import { SigninDto } from './dto/signin.dto';
 import { SignupDto } from './dto/signup.dto';
 
 @Controller('auth')
-@SetMetadata('IS_PUBLIC', true)
+@IsPublic()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
