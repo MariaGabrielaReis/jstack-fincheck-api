3e8bd1a873c938bf0513abd9b5bf885a52f0f40e
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-07-18 18:08:40 -0300

    style: change decorator type (function to const)

--- src/shared/decorators/IsPublic.ts
+++ src/shared/decorators/IsPublic.ts
@@ -2,6 +2,4 @@ import { SetMetadata } from '@nestjs/common';
 
 export const IS_PUBLIC_KEY = 'IS_PUBLIC';
 
-export function IsPublic() {
-  return SetMetadata(IS_PUBLIC_KEY, true);
-}
+export const IsPublic = () => SetMetadata(IS_PUBLIC_KEY, true);
