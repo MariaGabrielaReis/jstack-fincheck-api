83b73d5dc876d4c1845f2d21ebd599aa3f502047
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-07-24 17:45:36 -0300

    fix: rename categories repository

--- src/shared/database/database.module.ts
+++ src/shared/database/database.module.ts
@@ -2,7 +2,7 @@ import { Global, Module } from '@nestjs/common';
 
 import { PrismaService } from './prisma.service';
 import { BankAccountsRepository } from './repositories/bank-accounts.repositories';
-import { CategoriesRepository } from './repositories/categories.repositories copy';
+import { CategoriesRepository } from './repositories/categories.repositories';
 import { TransactionsRepository } from './repositories/transactions.repositories';
 import { UsersRepository } from './repositories/users.repositories';
 
